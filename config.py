5cb73f4fbc9cd0d64eb5fe8389c40d6b1239141c
notStonks <kuzewich.a@gmail.com>
2023-03-22 22:56:43 +0300

    set webhook

--- config.py
+++ config.py
@@ -9,5 +9,6 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
+URL_APP = os.environ.get("URL_APP")
 
 TOKEN = os.environ.get("TOKEN")
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import asyncio
 import logging
+import os
+
 from aiogram import Bot, Dispatcher, executor, types
-from creation_bot import dp
+
+import config
+from creation_bot import dp, bot
 import handlers
+
 # from database import db
 
 logging.basicConfig(level=logging.INFO)
@@ -13,13 +18,24 @@ async def on_startup(_):
     print("Bot online")
 
 
+async def on_startup_web(dp):
+    await bot.set_webhook(config.URL_APP)
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
 
 handlers.reg_handlers(dp)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-
-
-
-
-
+    #executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path='',
+        on_startup=on_startup_web,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
